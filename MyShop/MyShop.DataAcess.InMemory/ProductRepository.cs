3489a670912c0245aa30da61945ea8f8b10f83e4
ralphlagdameo <ralph.lagdameo@gmail.com>
2019-01-05 16:26:26 +0800

    Product Manager Built and Tested

--- MyShop/MyShop.DataAcess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAcess.InMemory/ProductRepository.cs
@@ -7,7 +7,7 @@ using System.Runtime.Caching;
 using MyShop.Core;
 using MyShop.Core.Models;
 
-namespace MyShop.DataAcess.InMemory
+namespace MyShop.DataAccess.InMemory
 {
     public class ProductRepository
     {
