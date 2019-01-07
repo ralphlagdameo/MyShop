2cf471c024ba5d52dfcfd55c7d4f537ee21947af
ralphlagdameo <ralph.lagdameo@gmail.com>
2019-01-07 19:31:24 +0800

    Implemented Generic Repository

--- MyShop/MyShop.DataAcess.InMemory/Properties/AssemblyInfo.cs
+++ MyShop/MyShop.DataAcess.InMemory/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyShop.DataAcess.InMemory")]
+[assembly: AssemblyTitle("MyShop.DataAccess.InMemory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyShop.DataAcess.InMemory")]
+[assembly: AssemblyProduct("MyShop.DataAccess.InMemory")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
